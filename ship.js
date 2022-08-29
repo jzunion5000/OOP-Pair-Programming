9465b84c9d7819baca1f9942cae6fe00567123cd
Jeff Zhang <jeffzhang@Jeffs-MBP.fios-router.home>
2022-08-29 14:51:47 -0400

    update

---
+++ ship.js
@@ -0,0 +1,20 @@
+function ship(healthPoints, speed){
+    return{
+        healthPoints,
+        speed
+    }
+}
+
+function Ship(healthPoints, speed){
+    this.healthPoints = healthPoints;
+    this.speed = speed;
+}
+
+class Ship{
+    constructor(healthPoints, speed){
+        this.healthPoints = healthPoints;
+        this.speed = speed;
+
+    }
+}
+
