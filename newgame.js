b03248d7866f249084f389ff10cb090453ca4dea
Andrea 'Dre' Arnold <a.arnold_08@yahoo.com>
2022-08-29 17:41:05 -0400

    reconfigured newgame

--- newgame.js
+++ newgame.js
@@ -3,29 +3,43 @@
 // when enemies consume pacman, pacman decreases 1 life
 // when life reaches 0 then it will tell the user the game is over
 
-function pacman(fruit, currentSize, currentSpeed,life) {
-    return {
-        life,
-        fruit,
-        currentSize,
-        currentSpeed,
-        fruitEaten: function(){
-            this.currentSize = this.currentSize += this.fruit;
-            this.currentSpeed = this.currentSpeed += this.fruit;
-        }
-    }
+function pacman(fruit, currentSize, currentSpeed, life) {
+  return {
+    life,
+    fruit,
+    currentSize,
+    currentSpeed,
+    fruitEaten: function () {
+      this.currentSize = this.currentSize += this.fruit;
+      this.currentSpeed = this.currentSpeed += this.fruit;
+    },
+    enemyEats: function (enemy) {
+      if (this.life === 0) {
+        return "GAME OVER";
+      } else {
+        this.life -= enemy.damage;
+      }
+    },
+  };
 }
 
-function enemies(){
-    return{
-        enemyEats: function(pacman){
-            if(pacman.life === 0){
-                return "GAME OVER"
-            }else{
-                pacman.life --
-            }
-        }
+function enemy(life, damage) {
+  return {
+    life,
+    damage,
+    enemyDamage: function(){
+        this.damage = this.life
     }
+  };
 }
-
-
+// function enemies(){
+//     return{
+//         enemyEats: function(pacman){
+//             if(pacman.life === 0){
+//                 return "GAME OVER"
+//             }else{
+//                 pacman.life --
+//             }
+//         }
+//     }
+// }
